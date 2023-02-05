92e334bda58030557a1fbf8ec39fd958573afd3b
Tony Liu <contactTonyLiu@gmail.com>
2023-02-05 01:13:30 -0800

    simplified imports

--- src/limes_x/__init__.py
+++ src/limes_x/__init__.py
@@ -1,1 +1,3 @@
+from .workflow import Workflow
 from .execution.modules import ModuleBuilder, ComputeModule, Item, JobContext, JobResult, Params
+from .execution.executors import Job, Executor, CloudExecutor
