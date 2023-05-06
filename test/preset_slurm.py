8470e4511cd2222dca5a67bbf7ae4906a48f7e66
Tony Liu <contactTonyLiu@gmail.com>
2023-05-05 19:28:30 -0700

    slurm preset

---
+++ test/preset_slurm.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+import limes_x as lx
+from limes_x.presets.slurm import Run
+
+modules = []
+modules += lx.LoadComputeModules("../../Limes-compute-modules/logistics")
+modules += lx.LoadComputeModules("../../Limes-compute-modules/metagenomics")
+
+Run(
+    modules = modules,
+    reference_folder="../../lx_ref",
+    workspace="./cache/test_workspace",
+    targets=[
+        lx.Item('metagenomic gzipped reads'),
+        lx.Item('metagenomic assembly'),
+        lx.Item("metagenomic bin"),
+        lx.Item("checkm stats"),
+        lx.Item('bin taxonomy table'),
+        lx.Item('assembly taxonomy table'),
+        lx.Item('genomic annotation'),
+    ],
+    given=[
+        lx.InputGroup(  
+            group_by=(lx.Item("sra accession"), "SRR19573024"), 
+            children={
+                lx.Item("username"): "Steven",
+            },
+        )
+    ],
+    allocation="alloc_code",
+    time="48:00:00",
+    # name="a test run",
+)
